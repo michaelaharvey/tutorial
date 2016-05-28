744dad1adb3c0052eec79127f073c4250d5f0b31
Michael Harvey <michael.harvey@pricespider.com>
2016-05-27 17:35:32 -0700

    first commit

---
+++ public/javascripts/vidzy.js
@@ -0,0 +1,9 @@
+var app = angular.module('Vidzy', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl: 'partials/home.html'
+	}).otherwise({
+		redirectTo: '/'
+	});
+}]);
