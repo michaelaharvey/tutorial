8ced06d416c55138d340a60c4bed1102649b6b34
Michael Harvey <michael.harvey@pricespider.com>
2016-06-01 15:45:10 -0700

    update

--- public/javascripts/vidzy.js
+++ public/javascripts/vidzy.js
@@ -1,9 +1,35 @@
-var app = angular.module('Vidzy', ['ngRoute']);
+var app = angular.module('Vidzy', ['ngResource', 'ngRoute']);
 
-app.config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/', {
-		templateUrl: 'partials/home.html'
-	}).otherwise({
-		redirectTo: '/'
-	});
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'HomeCtrl'
+        })
+        .when('/add-video', {
+            templateUrl: 'partials/video-form.html',
+            controller: 'AddVideoCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 }]);
+
+app.controller('HomeCtrl', ['$scope', '$resource', 
+    function($scope, $resource){
+        var Videos = $resource('/api/videos');
+        Videos.query(function(videos){
+            $scope.videos = videos;
+        });
+    }]);
+
+
+app.controller('AddVideoCtrl', ['$scope', '$resource', '$location',
+    function($scope, $resource, $location){
+        $scope.save = function(){
+            var Videos = $resource('/api/videos');
+            Videos.save($scope.video, function(){
+                $location.path('/');
+            });
+        };
+    }]);
